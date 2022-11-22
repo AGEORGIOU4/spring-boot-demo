42fd80f67baf0aaf2df9d82225a82c8d7cd0daf5
Andreas Georgiou <andreasg046@gmail.com>
2022-11-22 16:45:00 +0200

    Updates

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -30,10 +32,29 @@ public class StudentService {
         studentRepository.save(student);
     }
 
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"));
+
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            student.setEmail(email);
+        }
+    }
+
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
 
-        if(!exists) {
+        if (!exists) {
             throw new IllegalStateException("student with id " + studentId + " does not exists");
         }
         studentRepository.deleteById(studentId);
